500b7b108de1f2f385fe6dfc21fd2219a9596f39
Denis Boiarinoff <denis.boiarinoff@yandex.ru>
2016-06-06 11:29:24 +0600

    first commit

---
+++ SlideOutMenuStudyProject/LoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.m
+//  SlideOutMenuStudyProject
+//
+//  Created by Rhinoda3 on 06.06.16.
+//  Copyright © 2016 Rhinoda. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+	[self.navigationController.navigationBar setHidden:YES];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ SlideOutMenuStudyProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SlideOutMenuStudyProject
+//
+//  Created by Rhinoda3 on 06.06.16.
+//  Copyright © 2016 Rhinoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
